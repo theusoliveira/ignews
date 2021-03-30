e9747ffeb06868df82f23e537f5ff2deb7765397
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-03-30 09:27:00 -0300

    Router

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -8,8 +9,12 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news"/>
 
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
